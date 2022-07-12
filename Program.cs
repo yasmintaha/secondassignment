12894fc6159c3f9ed554801ce3b56065fa235785
yasmintaha <108139329+yasmintaha@users.noreply.github.com>
2022-07-13 00:32:30 +0300

    second assignment

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondassignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //perimeter and area  of the circle 
+            double r, per_circle,area;
+            double pi = 3.14;
+            Console.WriteLine("Input the radius of the circle:");
+            r=Convert.ToDouble(Console.ReadLine());
+            per_circle = 2 * pi * r;
+            Console.WriteLine("Perimeter of the circle  :{0}",per_circle);
+            area = pi * r * r;
+            Console.WriteLine("area of the circle  :{0}", area);
+            Console.Read();
+      
+
+        }
+    }
+}
